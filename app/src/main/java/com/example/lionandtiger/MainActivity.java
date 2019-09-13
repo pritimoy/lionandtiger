56f251a673f593e5562ce5dd6d16a4d129ed5d86
pritimoy <pritimoy54@gmail.com>
2019-09-14 02:20:29 +0600

    Added Images

--- app/src/main/java/com/example/lionandtiger/MainActivity.java
+++ app/src/main/java/com/example/lionandtiger/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.lionandtiger;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    enum Player {
+        ONE,TWO
+    }
+
+    Player currentPlayer = Player.ONE;
+    Player [] playerChoices = new Player[9];
+    int [][] winerRowsColumns= {{0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+    }
+    public void imageViewIsTapped(View imageView){
+        Log.i("MY TAG", "TApped image view");
+        ImageView tappedImageView = (ImageView) imageView;
+        tappedImageView.setTranslationX(-2000f);
+        int tiTag = Integer.parseInt(tappedImageView.getTag().toString());
+        playerChoices[tiTag] = currentPlayer;
+        if (currentPlayer == Player.ONE) {
+            tappedImageView.setImageResource(R.drawable.lion);
+            currentPlayer = Player.TWO;
+        }
+        else if (currentPlayer == Player.TWO){
+            tappedImageView.setImageResource(R.drawable.tiger);
+            currentPlayer = Player.ONE;
+        }
+
+        tappedImageView.animate().alpha(1f).translationXBy(2000f).rotation(3600).setDuration(1000);
+        Toast.makeText(this,tappedImageView.getTag().toString(),Toast.LENGTH_LONG).show();
     }
 }
